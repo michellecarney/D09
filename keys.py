a4aa79db2f3a6009ce759fa7201ec8bcedcf0a41
Michelle Carney <michelle.r.carney@gmail.com>
2016-08-15 23:34:41 -0700

    adding the ones from class so git status is happy

--- keys.py
+++ keys.py
@@ -44,3 +44,40 @@ languages = {'JavaScript': 'P',
              'Latin': 'N',
              'Romanian': 'N',
              'English': 'N'}
+
+from operator import itemgetter, attrgetter
+
+# def sort1(d):
+#     lst = sorted(sorted(d), key=d.__getitem___)
+#     print("1:")
+#     for x in lst:
+#         print("\t"+x)
+# sort1(languages)
+
+def sort1b(d):
+    import operator
+    lst = sorted(sorted(d.items()), key=operator.itemgetter(1))
+    print("sort 1: ")
+    for language, type_ in lst:
+        print("\t"+language)
+
+sort1b(languages)
+
+def sort2(d):
+    lst = sorted(d, key=len)
+    print("Sort 2:")
+    for item in lst:
+        print("\t" + item)
+sort2(languages)
+
+def sort3(d):
+    lst = sorted(sorted(d), key=last_char, reverse=True)
+    print("Sort 3:")
+    for item in lst:
+        print("\t" + item)
+
+def last_char(string):
+    return string[-1].lower()
+
+sort3(languages)
+
